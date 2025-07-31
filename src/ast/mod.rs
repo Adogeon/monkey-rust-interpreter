330666d7a97533032f8170c0593afaceee794dd5
Anh Dao <addao823@gmail.com>
2025-07-30 18:32:45 -0700

    First implment of asl

---
+++ src/ast/mod.rs
@@ -0,0 +1,59 @@
+use crate::token::Token;
+use std::boxed::Box;
+
+trait Node {
+    fn token_literal(&self) -> Option<&str>;
+}
+
+trait Expression: Node {
+    fn expression_node(&self);
+}
+
+trait Statement: Node {
+    fn statement_node(&self);
+}
+
+pub struct Program {
+    statements: Vec<Box<dyn Statement>>,
+}
+
+impl Node for Program {
+    fn token_literal(&self) -> Option<&str> {
+        if self.statements.len() > 0 {
+            self.statements[0].token_literal()
+        } else {
+            None
+        }
+    }
+}
+
+pub struct Identifier {
+    idt_token: Token,
+    value: String,
+}
+
+impl Node for Identifier {
+    fn token_literal(&self) -> Option<&str> {
+        Some(&self.idt_token.Literal)
+    }
+}
+
+impl Expression for Identifier {
+    fn expression_node(&self) {}
+}
+
+pub struct LetStatement {
+    stmt_token: Token,
+    name: Box<Identifier>,
+    value: Box<dyn Expression>,
+}
+
+impl Node for LetStatement {
+    fn token_literal(&self) -> Option<&str> {
+        Some(&self.stmt_token.Literal)
+    }
+}
+
+impl Statement for LetStatement {
+    fn statement_node(&self) {}
+}
--- src/repl/mod.rs
+++ src/repl/mod.rs
@@ -1,17 +1,28 @@
 use crate::lexer::Lexer;
 use crate::token::T_type;
-use std::io::{self, BufRead};
+use std::io::{self, BufRead, Write};
 
-pub fn start(in_handler: io::StdinLock, out_handler: io::StdoutLock) {
+pub fn start(mut in_handler: io::StdinLock, mut out_handler: io::StdoutLock) {
     let mut buffer = String::new();
 
     loop {
-        in_handler.read_line(&mut buffer);
-        let l = Lexer::new(&buffer);
-        while let tok = l.next_token() {
-            if tok.Type != T_type::EOF {
-                writeln!(out_handler, "Token: {}", tok);
+        write!(out_handler, ">>").unwrap();
+        out_handler.flush().unwrap();
+
+        buffer.clear();
+        if let Err(e) = in_handler.read_line(&mut buffer) {
+            writeln!(out_handler, "Error reading line: {}", e).unwrap();
+            break;
+        }
+
+        let mut l = Lexer::new(&buffer);
+
+        loop {
+            let tok = l.next_token();
+            if tok.Type == T_type::EOF {
+                break;
             }
+            writeln!(out_handler, "Token:{:?}", tok).unwrap();
         }
     }
 }
