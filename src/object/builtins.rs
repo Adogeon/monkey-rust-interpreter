15eb077d4684b9b30d58601b0b6a534d6c64be1e
Anh Dao <addao823@gmail.com>
2025-12-14 19:03:13 -0800

    Implement first, last, rest and push for Array

--- src/object/builtins.rs
+++ src/object/builtins.rs
@@ -25,6 +25,99 @@ pub fn builtins_fn(input: &str) -> Object {
                 }
             },
         })),
+        "first" => Object::BUILTIN(Rc::new(Builtin {
+            function: |parameter| {
+                if parameter.len() != 1 {
+                    return new_error(format!(
+                        "wrong number of arguments.got ={}, want 1",
+                        parameter.len()
+                    ));
+                };
+
+                match parameter[0].clone() {
+                    Object::ARRAY(arr) => match arr.first() {
+                        Some(v) => v.clone(),
+                        None => Object::NULL,
+                    },
+                    _ => {
+                        return new_error(format!(
+                            "argument to 'first' must be ARRAY, got {}",
+                            parameter[0].ob_type()
+                        ))
+                    }
+                }
+            },
+        })),
+        "last" => Object::BUILTIN(Rc::new(Builtin {
+            function: |parameter| {
+                if parameter.len() != 1 {
+                    return new_error(format!(
+                        "wrong number of arguments.got ={}, want 1",
+                        parameter.len()
+                    ));
+                };
+
+                match parameter[0].clone() {
+                    Object::ARRAY(arr) => match arr.last() {
+                        Some(v) => v.clone(),
+                        None => Object::NULL,
+                    },
+                    _ => {
+                        return new_error(format!(
+                            "argument to 'last' must be ARRAY, got {}",
+                            parameter[0].ob_type()
+                        ))
+                    }
+                }
+            },
+        })),
+        "rest" => Object::BUILTIN(Rc::new(Builtin {
+            function: |parameter| {
+                if parameter.len() != 1 {
+                    return new_error(format!(
+                        "wrong number of arguments.got ={}, want 1",
+                        parameter.len()
+                    ));
+                };
+
+                match parameter[0].clone() {
+                    Object::ARRAY(arr) => match arr.split_first() {
+                        Some((_first, rest)) => Object::ARRAY(rest.to_vec()),
+                        None => Object::NULL,
+                    },
+                    _ => {
+                        return new_error(format!(
+                            "argument to 'rest' must be ARRAY, got {}",
+                            parameter[0].ob_type()
+                        ))
+                    }
+                }
+            },
+        })),
+        "push" => Object::BUILTIN(Rc::new(Builtin {
+            function: |parameter| {
+                if parameter.len() != 2 {
+                    return new_error(format!(
+                        "wrong number of arguments.got ={}, want 2",
+                        parameter.len()
+                    ));
+                };
+
+                match parameter[0].clone() {
+                    Object::ARRAY(arr) => {
+                        let mut clone = arr.clone();
+                        clone.push(parameter[1].clone());
+                        Object::ARRAY(clone)
+                    }
+                    _ => {
+                        return new_error(format!(
+                            "argument to 'push' must be ARRAY, got {}",
+                            parameter[0].ob_type()
+                        ))
+                    }
+                }
+            },
+        })),
         _ => Object::NULL,
     }
 }
